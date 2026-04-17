61c67bdc81d133faa3137e303f252d7c997d861c
Olle Håkansson <ollhak@gmail.com>
2026-04-17 16:30:24 +0200

    Another attempt at fixing build..

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
+// import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://mysticore.net',
-	integrations: [mdx(), sitemap()],
+	integrations: [/*mdx(),*/ sitemap()],
 	fonts: [
 		{
 			provider: fontProviders.local(),
